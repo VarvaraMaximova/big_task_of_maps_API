18c64cf070e7c8169455f36a408fbf72bfa74e7a
VaryaMaximova <varmax2008@gmail.com>
2024-02-02 18:57:24 +0300

    Изменение масштаба карты по нажатию клавиш PqUp и PgDown

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ import sys
 import os
 
 import requests
+
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel
+from PyQt5.Qt import Qt
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.coord = 52.724508, 41.453693
-        self.map_zoom = 18
-        self.getImage()
+        self.map_zoom = 21
         self.initUI()
 
     def getImage(self):
@@ -22,7 +23,6 @@ class MainWindow(QMainWindow):
             print(map_request)
             print("Http статус:", response.status_code, "(", response.reason, ")")
             sys.exit(1)
-
         self.map_file = "map.png"
         with open(self.map_file, 'wb') as pngfile:
             pngfile.write(response.content)
@@ -30,15 +30,27 @@ class MainWindow(QMainWindow):
     def initUI(self):
         self.setGeometry(100, 100, 600, 600)
         self.setWindowTitle('Отображение карты')
-
-        self.pixmap = QPixmap(self.map_file)
         self.image = QLabel(self)
         self.image.move(30, 30)
         self.image.resize(540, 540)
-        self.image.setPixmap(self.pixmap)
+        self.updateLabel()
 
     def closeEvent(self, event):
         os.remove(self.map_file)
+
+    def keyPressEvent(self, event):
+        key = event.key()
+        if key == Qt.Key_PageUp and self.map_zoom < 21:
+            self.map_zoom += 1
+            self.updateLabel()
+        elif key == Qt.Key_PageDown and self.map_zoom > 1:
+            self.map_zoom -= 1
+            self.updateLabel()
+
+    def updateLabel(self):
+        self.getImage()
+        self.pixmap = QPixmap(self.map_file)
+        self.image.setPixmap(self.pixmap)
 
 
 if __name__ == '__main__':
